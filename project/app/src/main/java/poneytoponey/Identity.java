91d957905c7d1beba05ab8f6d4f2855bf91c2d55
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-04-14 16:27:20 +0200

    Make all Idendity methods throws RemoteException as necessary for published objects

--- project/app/src/main/java/poneytoponey/Identity.java
+++ project/app/src/main/java/poneytoponey/Identity.java
@@ -1,17 +1,18 @@
 package poneytoponey;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.UUID;
 
 public interface Identity extends Remote {
 
-    public void remoteAskForChat(String author, UUID chatId);
+    public void remoteAskForChat(String author, UUID chatId) throws RemoteException;
 
-    public void remoteApproveBackChat(UUID chatId);
+    public void remoteApproveBackChat(UUID chatId) throws RemoteException;
 
-    public void remoteRefuseChat(UUID chatId);
+    public void remoteRefuseChat(UUID chatId) throws RemoteException;
 
-    public void remoteSendMessageInChat(UUID chatId);
+    public void remoteSendMessageInChat(UUID chatId) throws RemoteException;
 
-    public void remoteCloseChat(UUID chatId);
+    public void remoteCloseChat(UUID chatId) throws RemoteException;
 }
