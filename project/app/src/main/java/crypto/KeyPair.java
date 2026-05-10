08f7c46fee517e2acf9b60a71a98cdb227ee7305
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-05-10 12:21:00 +0200

    Add todo implementation for RSA

--- project/app/src/main/java/crypto/KeyPair.java
+++ project/app/src/main/java/crypto/KeyPair.java
@@ -1,11 +1,20 @@
 package crypto;
 
+import java.security.PublicKey;
+
 // A RSA keypair
 public class KeyPair {
 	private java.security.KeyPair pair;
 
+	private KeyPair(java.security.KeyPair kp) {
+		this.pair = kp;
+	}
+
 	public static KeyPair loadFromFile(String path) {
 		// TODO
+		// PublicKey pub = PublicKe
+		// java.security.KeyPair = new java.security.KeyPair(pub, priv);
+		// return this;
 	}
 
 	public static KeyPair generateNewPair() {
--- project/app/src/main/java/crypto/RSA.java
+++ project/app/src/main/java/crypto/RSA.java
@@ -2,19 +2,34 @@ package crypto;
 
 // RSA-OAEP + RSA signature
 public class RSA implements Crypto {
+	@Override
 	public byte[] sign(byte[] buffer, String privateKey) {
 		// TODO
+		return "todo".getBytes();
 	}
 
+	@Override
 	public boolean verifySignature(byte[] signature, byte[] signedContent, String publicKey) {
 		// TODO
+		return false;
 	}
 
+	@Override
 	public byte[] encrypt(String text, String publicKey) {
 		// TODO
+		//
+		return "todo".getBytes();
 	}
 
+	@Override
 	public String decrypt(byte[] cipherText, String privateKey) {
 		// TODO
+		return "todo";
+	}
+
+	@Override
+	public KeyPair generateKeyPair() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'generateKeyPair'");
 	}
 }
